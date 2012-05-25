5140eb821542ac8e165406d34e1bea38195d2e6f
sander1 <sander@plexapp.com>
2012-05-25 23:36:08 +0200

    Use framework functions for regex and base64 decode; updated icon; remove some unnecessary xpaths; use background art on subitems too.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
-import re
-from base64 import b64decode
-
 NAME = "History Channel"
 ICON = "icon-default.png"
 ART = "art-default.jpg"
 BASE_URL = "http://www.history.com"
 
-####################################################################################################
+RE_MRSS = Regex('mrssData =\s+"([^"]+)')
 
+####################################################################################################
 def Start():
+
 	Plugin.AddPrefixHandler("/video/historychannel", MainMenu, NAME, ICON, ART)
 
 	Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
@@ -24,84 +23,66 @@ def Start():
 	VideoClipObject.thumb = R(ICON)
 
 	HTTP.CacheTime = CACHE_1HOUR
-	HTTP.Headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:10.0) Gecko/20100101 Firefox/10.0"
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:12.0) Gecko/20100101 Firefox/12.0'
 
 ####################################################################################################
-
 def MainMenu():
+
 	oc = ObjectContainer()
+
 	for show in HTML.ElementFromURL(BASE_URL+'/shows').xpath("//div[@id='all-shows-accordion']//div[@class='header']/span[@class='has-video']/preceding-sibling::span"):
 		showVideos = show.xpath('./parent::div/following-sibling::div[@class="content clearfix"]/div[@class="info"]//a[@class="watch more"]')[0].get('href')
-		if show.xpath('./parent::div/following-sibling::div[@class="content clearfix"]/ul[@class="nav"]/li')[0].text != None:
-			oc.add(DirectoryObject(
-				key = Callback(GetVideos, path = showVideos),
-				title = show.text))
-		else:
-			showMainPage = show.xpath('./parent::div/following-sibling::div[@class="content clearfix"]/ul[@class="nav"]/li/a[@class="more"]')[0].get('href')
-			oc.add(DirectoryObject(
-				key = Callback(GetVideos, path = showVideos, showMainPage = showMainPage),
-				title = show.text,
-				art = Callback(GetBackground, path = showMainPage)))
+		showMainPage = show.xpath('./parent::div/following-sibling::div[@class="content clearfix"]/ul[@class="nav"]/li/a[@class="more"]')[0].get('href')
+
+		oc.add(DirectoryObject(
+			key = Callback(GetVideos, path = showVideos, showMainPage = showMainPage),
+			title = show.text,
+			art = Callback(GetBackground, path = showMainPage)
+		))
+
 	return oc
 
 ####################################################################################################
+def GetVideos(path, showMainPage = None, isNestedPlaylist = False):
 
-def GetBackground(path):
-	try:
-		page = HTTP.Request(BASE_URL+path).content
-		bkgnd = page[page.find('background: url(')+16:]
-		bkgnd = bkgnd[:bkgnd.find(')')]
-
-		logo = HTML.ElementFromString(page).xpath('//div[@class="logo"]//img')[0].get('src')
-		if logo == None:
-			return DataObject(HTTP.Request('http://www.plexapp.tv/plugins/history/?image='+bkgnd, cacheTime=CACHE_1MONTH), 'image/jpeg')
-		else:
-			return DataObject(HTTP.Request('http://www.plexapp.tv/plugins/history/?image='+bkgnd+'&logo='+logo, cacheTime=CACHE_1MONTH), 'image/jpeg')
-	except:
-		return Redirect(R(ART))
-
-####################################################################################################
-
-def GetVideos(path, showMainPage = None, isNestedPlaylist=False):
-	if showMainPage != None:
-		oc = ObjectContainer(view_group = "InfoList", art = Callback(GetBackground, path=showMainPage))
-	else:
-		oc = ObjectContainer(view_group = "InfoList")
-
+	oc = ObjectContainer(view_group = "InfoList")
 	page = HTTP.Request(BASE_URL+path).content
 	playlists = HTML.ElementFromString(page).xpath('//li[contains(@class,"parent videos")]/ul/li/a')
-	
+
 	if playlists and not isNestedPlaylist:
 		for playlist in playlists:
 			oc.add(DirectoryObject(
-				key = Callback(GetVideos, path = playlist.xpath('.')[0].get('href'), isNestedPlaylist=True),
-				title = playlist.xpath('.')[0].text))
+				key = Callback(GetVideos, path = playlist.get('href'), showMainPage = showMainPage, isNestedPlaylist = True),
+				title = playlist.text,
+				art = Callback(GetBackground, path = showMainPage)
+			))
 	else:
-		mrssdata = re.search('mrssData =[ ]+"([^"]+)', page).group(1)
-		mrssdata =  String.Unquote(b64decode(mrssdata)).replace('media:','media-')
+		mrssdata = RE_MRSS.search(page).group(1)
+		mrssdata =  String.Unquote(String.Base64Decode(mrssdata)).replace('media:','media-')
 
 		for category in XML.ElementFromString(mrssdata).xpath("//item"):
 			video_url = category.xpath('./link')[0].text +'#'+ category.xpath('./media-category')[0].text
-
 			title = category.xpath('./title')[0].text
 			summary = category.xpath('./description')[0].text
 			thumb = category.xpath('./media-thumbnail')[0].get('url')
 			duration = int(category.xpath('./media-content')[0].get('duration')) * 1000
 
 			oc.add(VideoClipObject(
-		        url = video_url,
-		        title = title,
-		        summary = summary,
-		        thumb = Callback(GetThumb, path = thumb),
-		        duration = duration))
+				url = video_url,
+				title = title,
+				summary = summary,
+				thumb = Callback(GetThumb, path = thumb),
+				art = Callback(GetBackground, path = showMainPage),
+				duration = duration
+			))
 
 	if len(oc) == 0:
 		return MessageContainer("No Videos", "There aren't any videos available for this show")
 	return oc
 
 ####################################################################################################
-
 def GetThumb(path, thumb_type = "image/jpeg"):
+
 	if (path != None):
 		try:
 			return DataObject(HTTP.Request(path, cacheTime=CACHE_1MONTH), thumb_type)
@@ -109,3 +90,19 @@ def GetThumb(path, thumb_type = "image/jpeg"):
 			pass
 
 	return Redirect(R(ICON))
+
+####################################################################################################
+def GetBackground(path):
+
+	try:
+		page = HTTP.Request(BASE_URL+path).content
+		bkgnd = page[page.find('background: url(')+16:]
+		bkgnd = bkgnd[:bkgnd.find(')')]
+		logo = HTML.ElementFromString(page).xpath('//div[@class="logo"]//img')[0].get('src')
+
+		if logo == None:
+			return DataObject(HTTP.Request('http://www.plexapp.tv/plugins/history/?image='+bkgnd, cacheTime=CACHE_1MONTH), 'image/jpeg')
+		else:
+			return DataObject(HTTP.Request('http://www.plexapp.tv/plugins/history/?image='+bkgnd+'&logo='+logo, cacheTime=CACHE_1MONTH), 'image/jpeg')
+	except:
+		return Redirect(R(ART))
