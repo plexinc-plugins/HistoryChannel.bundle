2211e2a284210c51152bda6a26508934c1cbe172
shopgirl284 <shopgirl284@gmail.com>
2013-11-26 17:21:46 -0500

    fix issues

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -4,12 +4,12 @@ PREFIX = '/video/historychannel'
 BASE_URL = "http://www.history.com"
 H_SHOWS = "http://www.history.com/videos"
 H2_SHOWS = "http://www.history.com/videos/h2"
-# s= season m = carousel id and free = true or false to see those that are locked or unlocked
-# where %s is show link between history.com and videos
+# We pull the url for the show for the code below from the web page using regex
 # Ex http://www.history.com/api/html/shows/pawn-stars/videos?co=true&m=5189717d404fa&s=All&free=false
-API_URL = 'http://www.history.com/api/html%s/videos?co=true&m=%s&s=All&free=false'
+API_URL = 'http://www.history.com%s?co=true&m=%s&s=All&free=false'
 RE_CAROUSEL_ID = Regex("div id='(.+?)'><!--")
 RE_CAROUSEL_NAME = Regex('class="head-section1">(.+?)<')
+RE_API = Regex("freeFilter\[data-module-id=.+freefilter', '(.+?)'")
 ####################################################################################################
 def Start():
 
@@ -23,7 +23,9 @@ def MainMenu():
     oc = ObjectContainer()
     oc.add(DirectoryObject(key=Callback(Types, title='History Channel Shows', url=H_SHOWS), title='History Channel Shows')) 
     oc.add(DirectoryObject(key=Callback(Types, title='History Channel 2 Shows', url=H2_SHOWS), title='History Channel 2 Shows')) 
-    oc.add(DirectoryObject(key=Callback(Videos, title='Full Episodes', url=BASE_URL, carousel_id='524f1163e6d3a'), title='Full Episodes')) 
+    # Pull the full episode carousel id so it will not cause errors if it changes
+    carousel_id=HTML.ElementFromURL(BASE_URL).xpath('//h2[text()="Full Episodes"]/parent::div//@id')[0].split('_')[0]
+    oc.add(DirectoryObject(key=Callback(Videos, title='Full Episodes', show_url=BASE_URL, carousel_id=carousel_id), title='Full Episodes')) 
     return oc
 ####################################################################################################
 @route(PREFIX + '/types')
@@ -38,7 +40,7 @@ def Types(url, title):
         if not show_url.startswith('http://'):
             show_url = BASE_URL + show_url
         thumb = show.xpath('./a/img//@src')
-        oc.add(DirectoryObject(key = Callback(TypeMenu, show_url=show_url, title=name, thumb=thumb), title=name, thumb=Resource.ContentsOfURLWithFallback(url=thumb)))
+        oc.add(DirectoryObject(key = Callback(TypeMenu, show_url=show_url, title=name, thumb=thumb), title=name, thumb=Resource.ContentsOfURLWithFallback(thumb)))
 
     return oc
 
@@ -55,7 +57,7 @@ def TypeMenu(title, show_url, thumb):
         except:
             title = section.xpath('./div/h3//text()')[0].strip()
         carousel_id = section.xpath('.//@id')[0]
-        oc.add(DirectoryObject(key = Callback(Videos, show_url=show_url, title=title, carousel_id=carousel_id), title=title, thumb=Resource.ContentsOfURLWithFallback(url=thumb)))
+        oc.add(DirectoryObject(key = Callback(Videos, show_url=show_url, title=title, carousel_id=carousel_id), title=title, thumb=Resource.ContentsOfURLWithFallback(thumb)))
 
     return oc
 
@@ -65,26 +67,45 @@ def Videos(show_url, title, carousel_id):
 
     oc = ObjectContainer(title2=title)
 
-    show_part = show_url.split(BASE_URL)[1].split('/videos')[0]
-    content = HTTP.Request(API_URL %(show_part, carousel_id)).content
-    content = "[%s]" % content
-    content_json = JSON.ObjectFromString(content)
-    data = content_json[0][carousel_id]
-    data = HTML.ElementFromString(data)
+    show_content = HTTP.Request(show_url).content
+    # Found that the main page no longer has a an api for its carousels. 
+    # This will try to find the carousel api information in the page to produce carousels
+    # If not, it will pull the carousel info straight from the html page
+    try:
+        show_api = RE_API.search(show_content).group(1)
+        content = HTTP.Request(API_URL %(show_api, carousel_id)).content
+        content = "[%s]" % content
+        content_json = JSON.ObjectFromString(content)
+        data = content_json[0][carousel_id]
+        data = HTML.ElementFromString(data)
+    except:
+        data_section = HTML.ElementFromString(show_content).xpath('//div[contains(@id,"%s")]' %carousel_id)[0]
+        raw_data = HTML.StringFromElement(data_section)
+        data = HTML.ElementFromString(raw_data)
         
     for episode in data.xpath('//li[contains(@class,"slider-item")]/a'):
         url = episode.xpath('.//@data-href')[0]
+        if not url.startswith('http://'):
+            url = BASE_URL + url
         data_status = episode.xpath('.//@data-status')[0]
         title = episode.xpath('.//@data-original-title')[0]
         if data_status == 'locked':
             continue
-        duration = episode.xpath('.//@data-duration')[0].replace('min', '')
-        duration = int(duration) * 60000
-        rating = episode.xpath('.//@data-rating')[0]
+        # Found one video with an empty duration, since it has to be in a try, putting whole pull there to be safe
         try:
-            thumb = episode.xpath('./span/span/img//@src')[0]
+            duration = episode.xpath('.//@data-duration')[0].replace('min', '')
+            duration = int(duration) * 60000
         except:
-            thumb = None
+            duration = 0
+        # Some sections do not have a rating
+        try:
+            rating = episode.xpath('.//@data-rating')[0]
+        except:
+            rating = None
+        try:
+            thumb = episode.xpath('.//img//@src')[0]
+        except:
+            thumb = episode.xpath('.//img//@data-src')[0]
         description = episode.xpath('.//@data-content')[0]
         summary = HTML.ElementFromString(description).xpath('./p//text()')[0]
 
@@ -93,7 +114,7 @@ def Videos(show_url, title, carousel_id):
             title = title,
             content_rating = rating,
             summary = summary,
-            thumb = Resource.ContentsOfURLWithFallback(url=thumb)
+            thumb = Resource.ContentsOfURLWithFallback(thumb)
         ))
 
     if len(oc) < 1:
