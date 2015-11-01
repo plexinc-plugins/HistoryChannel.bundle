b02ab9d74f061043a4daf7c35146c598cd076ddd
unknown (author) <meriko@hotmail.com>
2015-11-01 12:49:17 +0100

    Add season navigation

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,7 +1,7 @@
 TITLE = 'History Channel'
 PREFIX = '/video/historychannel'
 
-SHOWS = "http://wombatapi.aetv.com/shows2/history"
+SHOWS = 'http://wombatapi.aetv.com/shows2/history'
 SIGNATURE_URL = 'http://servicesaetn-a.akamaihd.net/jservice/video/components/get-signed-signature?url=%s'
 SMIL_NS = {"a":"http://www.w3.org/2005/SMIL21/Language"}
 
@@ -52,7 +52,8 @@ def Shows(title, network):
         oc.add(
             TVShowObject(
                 key = Callback(
-                    Episodes,
+                    Seasons,
+                    show_id = item['showID'],
                     show_title = item['detailTitle'],
                     episode_url = item['episodeFeedURL'],
                     clip_url = item['clipFeedURL'],
@@ -71,19 +72,60 @@ def Shows(title, network):
     return oc
 
 ####################################################################################################
-@route(PREFIX + '/episodes')
-def Episodes(show_title, episode_url, clip_url, show_thumb):
-    json_data = JSON.ObjectFromURL(episode_url)
-    
-    if json_data['totalNumber'] < 1:
-        return ObjectContainer(header='Empty', message='This show does not have any unlocked videos available.') 
-    
+@route(PREFIX + '/seasons')
+def Seasons(show_id, show_title, episode_url, clip_url, show_thumb):
+
     oc = ObjectContainer(title2=show_title)
     
+    json_data = JSON.ObjectFromURL(episode_url + '&filter_by=isBehindWall&filter_value=false')
+    
+    seasons = {}
     for item in json_data['Items']:
-        if not (item['isBehindWall'] == 'false'):
-            continue
+        if 'season' in item:
+            if not int(item['season']) in seasons:
+                seasons[int(item['season'])] = 1
+            else:
+                seasons[int(item['season'])] = seasons[int(item['season'])] + 1
+    
+    Log(seasons)
+    for season in seasons:
+        oc.add(
+            SeasonObject(
+                key = Callback(
+                    Episodes,
+                    show_title = show_title,
+                    episode_url = episode_url,
+                    clip_url = clip_url,
+                    show_thumb = show_thumb,
+                    season = season
+                ),
+                title = 'Season %s' % season,
+                rating_key = show_id + str(season),
+                index = int(season),
+                episode_count = seasons[season],
+                thumb = show_thumb
+            )
+        )
+ 
+    if len(oc) < 1:
+        return ObjectContainer(header='Empty', message='This show does not have any unlocked videos available.')
+    else:
+        oc.objects.sort(key = lambda obj: obj.index, reverse = True)
+        return oc 
+    
 
+####################################################################################################
+@route(PREFIX + '/episodes')
+def Episodes(show_title, episode_url, clip_url, show_thumb, season):
+
+    oc = ObjectContainer(title2=show_title)
+    json_data = JSON.ObjectFromURL(episode_url + '&filter_by=isBehindWall&filter_value=false')
+    
+    for item in json_data['Items']:
+        if 'season' in item:
+            if not int(item['season']) == int(season):
+                continue
+        
         url = item['playURL_HLS']
         title = item['title']
         summary = item['description'] if 'description' in item else None
@@ -118,15 +160,12 @@ def Episodes(show_title, episode_url, clip_url, show_thumb):
             )
         )
     
-    try:
-        oc.objects.sort(key = lambda obj: obj.index)
-    except:
-        pass
+    oc.objects.sort(key = lambda obj: obj.index)
     
     return oc
 
 ###################################################################################################
-@route(PREFIX + '/createepisodeclipobject', duration=int, index=int, season=int, include_container=bool)
+@route(PREFIX + '/createepisodeobject', duration=int, index=int, season=int, include_container=bool)
 def CreateEpisodeObject(url, title, summary, thumb, art, show, duration, originally_available_at, index, season, include_container=False):
 
     episode_obj = EpisodeObject(
@@ -171,8 +210,9 @@ def CreateEpisodeObject(url, title, summary, thumb, art, show, duration, origina
         return episode_obj
 
 ###################################################################################################
+@route(PREFIX + '/playvideo.m3u8')
 @indirect
-def PlayVideo(url):
+def PlayVideo(url, **kwargs):
 
     unsigned_smil_url = url.split('?')[0]
     signature = HTTP.Request(SIGNATURE_URL % String.Quote(unsigned_smil_url)).content
